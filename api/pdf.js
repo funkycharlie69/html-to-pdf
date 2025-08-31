322b003eaf4b11357f2c56ae2c4733b374762dc1
funkycharlie69 <charles.murillon@gmail.com>
2025-08-31 19:14:05 +0200

    Update pdf.js

--- api/pdf.js
+++ api/pdf.js
@@ -12,16 +12,18 @@ export default async function handler(req, res) {
       args: chromium.args,
       defaultViewport: chromium.defaultViewport,
       executablePath: await chromium.executablePath(),
-      headless: chromium.headless,
+      headless: chromium.headless
     });
 
     const page = await browser.newPage({ viewport: { width: 1240, height: 1754 } });
     await page.setContent(html, { waitUntil: "networkidle0" });
+
     const pdf = await page.pdf({
       format: "A4",
       printBackground: true,
       margin: { top: "12mm", right: "12mm", bottom: "16mm", left: "12mm" }
     });
+
     await browser.close();
 
     res.setHeader("Content-Type", "application/pdf");
